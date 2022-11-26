6b5b542f5249db439f9061d2382f23df1088b771
unknown <ibtissam.aoulek@etu.uae.ac.ma>
2022-11-26 23:52:51 +0100

    Add Project

---
+++ src/main/java/org/lsi/people/ticket/management/app/PeopleManagementSpringBootDataApp1Application.java
@@ -0,0 +1,13 @@
+package org.lsi.people.ticket.management.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementSpringBootDataApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagementSpringBootDataApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/lsi/people/ticket/management/app/PeopleManagementSpringBootDataApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lsi.people.ticket.management.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementSpringBootDataApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
